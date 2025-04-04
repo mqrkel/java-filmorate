168bf5126a8824b8f40ed47990817278930c0615
mqrkel <meredial1997@yandex.ru>
2025-04-04 12:43:05 +0300

    Sprint 11. Перенос проверки пользователя на слой сервиса

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmMemoryStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
 
